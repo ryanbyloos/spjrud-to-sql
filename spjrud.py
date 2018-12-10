b6828f8cfedd26f0a0241500427e6870b6606a49
Ryan B <ryan@monolith.localdomain>
2018-12-10 10:29:53 +0100

    Defining operations

---
+++ spjrud.py
@@ -0,0 +1,27 @@
+class Relation():
+    def __init__(self):
+        pass
+
+class Select(Relation):
+    def __init__(self):
+        pass
+
+class Project(Relation):
+    def __init__(self):
+        pass
+
+class Joint(Relation):
+    def __init__(self):
+        pass
+
+class Rename(Relation):
+    def __init__(self):
+        pass
+
+class Union(Relation):
+    def __init__(self):
+        pass
+
+class Diff(Relation):
+    def __init__(self):
+        pass
