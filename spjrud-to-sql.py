ca2f4073229edac0c79521101204d2988c077040
Ryan B <ryan@monolith.localdomain>
2018-12-10 10:54:48 +0100

    add main file

---
+++ spjrud-to-sql.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+from src.Relation import Relation
+from src.Select import Select
+from src.Project import Project
+from src.Joint import Joint
+from src.Rename import Rename
+from src.Union import Union
+from src.Diff import Diff
+
