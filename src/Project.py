f49830fda59197ab40e829af0241bd4bece778b7
Ryan B <ryan@monolith.localdomain>
2018-12-13 23:03:21 +0100

    check_args() implementation

--- src/Project.py
+++ src/Project.py
@@ -12,11 +12,24 @@ class Project(Relation):
         if not isinstance(subrelation, Relation):
             raise TypeError('The subrelation must be a relation.')
 
+        if not self.check_args():
+            raise TypeError('One attribute isn\'t in the relation.')
+
         for e in attr_list:
             self.attr+=e.name
             self.attr+=", "
         self.attr = self.attr[:-2] # There is one useless ", "
-        self.subrelation = subrelation
+        self.subrelation, self.attr_list = subrelation, attr_list
+
+    def check_args(self):
+        argtype = {}
+        details = Database.db.c.execute("PRAGMA table_info({0})".format(self.subrelation.compile()))
+        for i in details:
+            argtype[i[1]] = i[2]
+        for j in self.attr_list:
+            if argtype[j] == None:
+                return False
+        return True
         
     def compile(self):
         return "SELECT {0} FROM ({1})".format(self.attr, self.subrelation.compile())
