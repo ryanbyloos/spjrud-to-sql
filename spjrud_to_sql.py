d04cbb196c852d3564f5a457f13da58a32d45e05
Ryan B <ryan@monolith.localdomain>
2018-12-14 13:19:20 +0100

    pep8 formating

--- spjrud_to_sql.py
+++ spjrud_to_sql.py
@@ -1,15 +1,15 @@
 import sqlite3 as sql
 
-from src.Select     import Select   as S
-from src.Project    import Project  as P
-from src.Join       import Join     as J
-from src.Rename     import Rename   as R
-from src.Union      import Union    as U
-from src.Diff       import Diff     as D
-from src.Relation   import Relation as Rel
-from src.Attribute  import Attribute as Attr
-from src.Constant   import Constant as Const
-from src.Database   import Database, db
+from src.Select import Select as S
+from src.Project import Project as P
+from src.Join import Join as J
+from src.Rename import Rename as R
+from src.Union import Union as U
+from src.Diff import Diff as D
+from src.Relation import Relation as Rel
+from src.Attribute import Attribute as Attr
+from src.Constant import Constant as Const
+from src.Database import Database, db
 
 
 def setup(db_name):
