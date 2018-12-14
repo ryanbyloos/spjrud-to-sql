055992a8702387043acecf72e1c657d7d266b1ef
Ryan B <ryan@monolith.localdomain>
2018-12-14 17:11:34 +0100

    added default extension

--- src/Database.py
+++ src/Database.py
@@ -3,7 +3,7 @@ import sqlite3 as sql
 
 class Database:
 
-    def __init__(self, db_name, ext):
+    def __init__(self, db_name, ext='.db'):
         if db_name != None:
             self.db_name = db_name
             self.conn = sql.connect(db_name+ext)
