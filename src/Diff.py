4b028634729a3a5e578fb40edd07f184f6e279e3
Ryan B <ryan@monolith.localdomain>
2018-12-14 10:33:27 +0100

    improved check_args()

--- src/Diff.py
+++ src/Diff.py
@@ -1,5 +1,6 @@
 from src.Relation import Relation
 from src.Attribute import Attribute
+from src.Database import Database, db
 
 class Diff(Relation):
     def __init__(self, subrelation1, subrelation2):
@@ -9,8 +10,15 @@ class Diff(Relation):
     def check_args(self):
         if not (isinstance(self.subrelation1, Relation) or isinstance(self.subrelation2, Relation)):
             raise TypeError('The subrelations must be relations')
-
         
+        argtype = {}
+        Database.db.c.execute("PRAGMA table_info({0})".format(self.subrelation1.compile()))
+        len_r1 = len(Database.db.c.fetchall())
+        Database.db.c.execute("PRAGMA table_info({0})".format(self.subrelation2.compile()))
+        len_r2 = len(Database.db.c.fetchall())
+
+        if len_r1 != len_r2:
+            raise ValueError('The two subrelations must have the same amount of columns.')
             
     def compile(self):
         return "SELECT * FROM {0} EXCEPT SELECT * FROM {1}".format(self.subrelation1.compile(), self.subrelation2.compile())
